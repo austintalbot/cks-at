d60851c7d961a51ed018998302bb48de19f1d0e1
Austin Talbot <austintalbot@gmail.com>
2025-03-23 12:07:44 -0600

    Add network policies and update scripts for connectivity testing

--- setup-tools.sh
+++ setup-tools.sh
@@ -8,6 +8,28 @@ chmod +x kubecolor
 sudo mv kubecolor /usr/local/bin/
 rm kubecolor_0.5.0_linux_amd64.tar.gz
 
+
+
+echo "install krew"
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+kubectl krew install cyclonus neat np-viewer sniff view-secret view-utilization view-webhook who-can
+# Add Krew to PATH in .bashrc
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >>~/.bashrc
+
+
+
+
 echo "setup aliases and completion"
 
 echo "source <(kubectl completion bash)" >>~/.bashrc
--- test-connectivity.sh
+++ test-connectivity.sh
@@ -5,6 +5,7 @@ function test_connectivity() {
     local target=$2
     local namespace=$3
     local target_namespace=$4
+    echo "testing $source --> $target.$target_namespace.svc.cluster.local"
     local result=$(kubecolor -n $namespace exec deployments/$source -- wget -qSO- --timeout=1 $target.$target_namespace.svc.cluster.local 2>&1 | grep "HTTP/" | awk '{print $2}')
     if [ "$result" == "200" ]; then
         echo -e "\e[32m$source--> $target.$target_namespace.svc.cluster.local: $result\e[0m" | column -t
@@ -17,82 +18,88 @@ echo "testing network connectivity"
 
 echo "==========================="
 echo "app1 -> app2, manager1, manager2, data-001, data-002, processor-a100, processor-a200"
-test_connectivity app1 app2 app app
-test_connectivity app1 manager1 app app
-test_connectivity app1 manager2 app app
-test_connectivity app1 data-001 app data
-test_connectivity app1 data-002 app data
-test_connectivity app1 processor-a100 app data
-test_connectivity app1 processor-a200 app data
+test_connectivity app1 app2 app app &
+test_connectivity app1 manager1 app app &
+test_connectivity app1 manager2 app app &
+test_connectivity app1 data-001 app data &
+test_connectivity app1 data-002 app data &
+test_connectivity app1 processor-a100 app data &
+test_connectivity app1 processor-a200 app data &
+
 
 echo "==========================="
 echo "app2 -> app1, manager1, manager2, data-001, data-002, processor-a100, processor-a200"
-test_connectivity app2 app1 app app
-test_connectivity app2 manager1 app app
-test_connectivity app2 manager2 app app
-test_connectivity app2 data-001 app data
-test_connectivity app2 data-002 app data
-test_connectivity app2 processor-a100 app data
-test_connectivity app2 processor-a200 app data
+test_connectivity app2 app1 app app &
+test_connectivity app2 manager1 app app &
+test_connectivity app2 manager2 app app &
+test_connectivity app2 data-001 app data &
+test_connectivity app2 data-002 app data &
+test_connectivity app2 processor-a100 app data &
+test_connectivity app2 processor-a200 app data &
+
 
 echo "==========================="
 echo "manager1 -> app1, app2, manager2, data-001, data-002, processor-a100, processor-a200"
-test_connectivity manager1 app1 app app
-test_connectivity manager1 app2 app app
-test_connectivity manager1 manager2 app app
-test_connectivity manager1 data-001 app data
-test_connectivity manager1 data-002 app data
-test_connectivity manager1 processor-a100 app data
-test_connectivity manager1 processor-a200 app data
+test_connectivity manager1 app1 app app &
+test_connectivity manager1 app2 app app &
+test_connectivity manager1 manager2 app app &
+test_connectivity manager1 data-001 app data &
+test_connectivity manager1 data-002 app data &
+test_connectivity manager1 processor-a100 app data &
+test_connectivity manager1 processor-a200 app data &
+
 
 echo "==========================="
 echo "manager2 -> app1, app2, manager1, data-001, data-002, processor-a100, processor-a200"
-
-test_connectivity manager2 app1 app app
-test_connectivity manager2 app2 app app 
-test_connectivity manager2 manager1 app app
-test_connectivity manager2 data-001 app data
-test_connectivity manager2 data-002 app data
-test_connectivity manager2 processor-a100 app data
-test_connectivity manager2 processor-a200 app data
+test_connectivity manager2 app1 app app &
+test_connectivity manager2 app2 app app &
+test_connectivity manager2 manager1 app app &
+test_connectivity manager2 data-001 app data &
+test_connectivity manager2 data-002 app data &
+test_connectivity manager2 processor-a100 app data &
+test_connectivity manager2 processor-a200 app data &
 
 
 echo "==========================="
 echo "data-001 -> app1, app2, manager1, manager2, data-002, processor-a100, processor-a200"
-test_connectivity data-001 app1 data app
-test_connectivity data-001 app2 data app 
-test_connectivity data-001 manager1 data app
-test_connectivity data-001 manager2 data app
-test_connectivity data-001 data-002 data data
-test_connectivity data-001 processor-a100 data data
-test_connectivity data-001 processor-a200 data data
+test_connectivity data-001 app1 data app &
+test_connectivity data-001 app2 data app &
+test_connectivity data-001 manager1 data app &
+test_connectivity data-001 manager2 data app &
+test_connectivity data-001 data-002 data data &
+test_connectivity data-001 processor-a100 data data &
+test_connectivity data-001 processor-a200 data data &
+
 
 echo "==========================="
 echo "data-002 -> app1, app2, manager1, manager2, data-001, processor-a100, processor-a200"
-test_connectivity data-002 app1 data app
-test_connectivity data-002 app2 data app
-test_connectivity data-002 manager1 data app
-test_connectivity data-002 manager2 data app
-test_connectivity data-002 data-001 data data
-test_connectivity data-002 processor-a100 data data
-test_connectivity data-002 processor-a200 data data
+test_connectivity data-002 app1 data app &
+test_connectivity data-002 app2 data app &
+test_connectivity data-002 manager1 data app &
+test_connectivity data-002 manager2 data app &
+test_connectivity data-002 data-001 data data &
+test_connectivity data-002 processor-a100 data data &
+test_connectivity data-002 processor-a200 data data &
+
 
 echo "==========================="
 echo "processor-a100 -> app1, app2, manager1, manager2, data-001, data-002, processor-a200"
-test_connectivity processor-a100 app1 data app
-test_connectivity processor-a100 app2 data app
-test_connectivity processor-a100 manager1 data app
-test_connectivity processor-a100 manager2 data app
-test_connectivity processor-a100 data-001 data data
-test_connectivity processor-a100 data-002 data data
-test_connectivity processor-a100 processor-a200 data data
+test_connectivity processor-a100 app1 data app &
+test_connectivity processor-a100 app2 data app &
+test_connectivity processor-a100 manager1 data app &
+test_connectivity processor-a100 manager2 data app &
+test_connectivity processor-a100 data-001 data data &
+test_connectivity processor-a100 data-002 data data &
+test_connectivity processor-a100 processor-a200 data data &
+
 
 echo "==========================="
 echo "processor-a200 -> app1, app2, manager1, manager2, data-001, data-002, processor-a100"
-test_connectivity processor-a200 app1 data app
-test_connectivity processor-a200 app2 data app
-test_connectivity processor-a200 manager1 data app
-test_connectivity processor-a200 manager2 data app
-test_connectivity processor-a200 data-001 data data
-test_connectivity processor-a200 data-002 data data
-test_connectivity processor-a200 processor-a100 data data
+test_connectivity processor-a200 app1 data app &
+test_connectivity processor-a200 app2 data app &
+test_connectivity processor-a200 manager1 data app &
+test_connectivity processor-a200 manager2 data app &
+test_connectivity processor-a200 data-001 data data &
+test_connectivity processor-a200 data-002 data data &
+test_connectivity processor-a200 processor-a100 data data &
+wait
